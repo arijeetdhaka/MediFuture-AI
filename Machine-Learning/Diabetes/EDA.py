efac69e9634cebdc8084bd78bcffa8492336b4d7
Arijeet Dhaka <dhakaarijeet72@gmail.com>
2026-07-21 19:29:53 +0530

    colmpeted EDA

--- Machine-Learning/Diabetes/EDA.py
+++ Machine-Learning/Diabetes/EDA.py
@@ -5,5 +5,24 @@ print("\nfirst 5 rows:\n",df.head())
 print("\nno of columns:\n",df.columns)
 print("\ndatatypes of columns:\n",df.dtypes)
 print("\nchecking for null values:\n",df.isnull().sum())
+print("\nchecking for duplicate values:\n",df.duplicated().sum())
+print("\nstatistics of dataset:\n",df.describe())
+print("\nPercentage Distribution of target variable:\n")
+print(df["Outcome"].value_counts(normalize=True)*100)
+columns = ["Glucose",
+           "BloodPressure",
+           "SkinThickness",
+           "Insulin",
+           "BMI"]
 
+print("\nPercentage of Zero Values:\n")
+
+for column in columns:
+    zero_percentage = ((df[column] == 0).sum()/len(df))*100
+
+    print(f"{column} : {zero_percentage:.2f}%")
+
+#correlation analysis
+print("\nCorrelation Analysis:\n")
+print(df.corr())
 
